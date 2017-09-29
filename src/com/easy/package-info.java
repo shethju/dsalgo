952bde745e98ed7ef291a3a2256d4d9b1975938f
Admin <shethju@gmail..com>
2017-09-29 01:06:45 -0700

    Test

---
+++ src/com/easy/package-info.java
@@ -0,0 +1,17 @@
+package com.easy;
+
+public class HammingDistance {
+  public static int hammingDistance(int x, int y) {
+    int distance = 0;
+    String xStr = Integer.toBinaryString(x);
+    String yStr = Integer.toBinaryString(y);
+    // for ()
+    return distance;
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hamming distance is " + hammingDistance(1, 2));
+  }
+
+}
